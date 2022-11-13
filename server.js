086e9a82d4a5512d1f89b658125fbdb1baf85b18
Dane Cronin <danecronin@gmail.com>
2022-11-13 09:37:50 -0700

    deploying to heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(require('./routes'));
 
 
 //connecting  mongoose 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
